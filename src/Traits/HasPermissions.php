1e7a8d046706de2468239269c35e8ed17b9e05c8
Dadan Satria <dadan@orderonline.id>
2024-03-22 10:52:33 +0700

    fix: handle current role when sync permission

--- src/Traits/HasPermissions.php
+++ src/Traits/HasPermissions.php
@@ -18,82 +18,86 @@ use function is_string;
  */
 trait HasPermissions
 {
-	/**
-	 * Grant the given permission(s) to a role.
-	 *
-	 * @param ...$permissions
-	 * @return Role|HasPermissions
-	 * @throws ReflectionException
-	 */
-	public function givePermissionTo(...$permissions): self
-	{
-		$inputPermissions = collect($permissions)->map(function ($permission) {
-			$dataPermission = $this->getStoredPermission($permission);
-			return [
-				'id' => new ObjectId($dataPermission->id),
-				'code' => $permission
-			];
-		})->whereNotIn('code', collect($this->permissions)->pluck('code'))
-			->toArray();
+    /**
+     * Grant the given permission(s) to a role.
+     *
+     * @param ...$permissions
+     * @return Role|HasPermissions
+     * @throws ReflectionException
+     */
+    public function givePermissionTo(...$permissions): self
+    {
+        $inputPermissions = collect($permissions)->map(function ($permission) {
+            $dataPermission = $this->getStoredPermission($permission);
+            return [
+                'id' => new ObjectId($dataPermission->id),
+                'code' => $permission
+            ];
+        })->whereNotIn('code', collect($this->permissions)->pluck('code'))
+            ->toArray();
 
-		if (!empty($inputPermissions)) {
-			$this->permissions = collect($this->permissions)->merge($inputPermissions)->toArray();
-			$this->save();
-		}
+        if (!empty($inputPermissions)) {
+            $this->permissions = collect($this->permissions)->merge($inputPermissions)->toArray();
+            $this->save();
+        }
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * Remove all current permissions and set the given ones.
-	 *
-	 * @param ...$permissions
-	 * @return Role|HasPermissions
-	 * @throws ReflectionException
-	 */
-	public function syncPermissions(...$permissions): self
-	{
-		$inputPermissions = collect($permissions)->map(function ($permission) {
-			$dataPermission = $this->getStoredPermission($permission);
-			return [
-				'id' => new ObjectId($dataPermission->id),
-				'code' => $permission
-			];
-		})->toArray();
+    /**
+     * Remove all current permissions and set the given ones.
+     *
+     * @param ...$permissions
+     * @return Role|HasPermissions
+     * @throws ReflectionException
+     */
+    public function syncPermissions(...$permissions): self
+    {
+        $inputPermissions = collect($permissions)->map(function ($permission) {
+            $dataPermission = $this->getStoredPermission($permission);
+            return [
+                'id' => new ObjectId($dataPermission->id),
+                'code' => $permission
+            ];
+        });
+        if (!$this instanceof Role && !empty($this->role)) {
+            $inputPermissions = $inputPermissions
+                ->merge($this->permissions)
+                ->unique();
+        }
+        $this->permissions = $inputPermissions->toArray();
+        $this->save();
 
-		$this->permissions = $inputPermissions;
-		$this->save();
+        return $this;
+    }
 
-		return $this;
-	}
+    /**
+     * Revoke the given permission.
+     *
+     * @param ...$permissions
+     * @return Role|HasPermissions
+     */
+    public function revokePermissionTo(...$permissions): self
+    {
+        $this->permissions = collect($this->permissions ?? [])
+            ->whereNotIn('code', $permissions)
+            ->all();
+        $this->save();
+        return $this;
+    }
 
-	/**
-	 * Revoke the given permission.
-	 *
-	 * @param ...$permissions
-	 * @return Role|HasPermissions
-	 */
-	public function revokePermissionTo(...$permissions): self
-	{
-		$this->permissions = collect($this->permissions ?? [])
-			->whereNotIn('code', $permissions)
-			->all();
-		$this->save();
-		return $this;
-	}
+    /**
+     * @param $permission
+     * @return \Illuminate\Database\Eloquent\Builder|mixed|Model
+     * @throws ReflectionException
+     */
+    protected function getStoredPermission($permission): mixed
+    {
+        $guard = (new Guard())->getDefaultName();
+        if (is_string($permission)) {
+            return Permission::findByCode($permission, $guard);
+        }
 
-	/**
-	 * @param $permission
-	 * @return \Illuminate\Database\Eloquent\Builder|mixed|Model
-	 * @throws ReflectionException
-	 */
-	protected function getStoredPermission($permission): mixed
-	{
-		$guard = (new Guard())->getDefaultName();
-		if (is_string($permission)) {
-			return Permission::findByCode($permission, $guard);
-		}
-
-		return $permission;
-	}
+        return $permission;
+    }
 }
